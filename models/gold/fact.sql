5e136e8887a01a6cdf12be0e43831fbf7b20dd8c
Aqil Aiman <aqilaiman058@gmail.com>
2026-03-07 07:03:41 +0000

    setup bronze silver gold models

---
+++ models/gold/fact.sql
@@ -0,0 +1,11 @@
+select
+    booking_id,
+    listing_id,
+    booking_date,
+    booking_status,
+    nights_booked,
+    booking_amount,
+    cleaning_fee,
+    service_fee,
+    created_at
+from {{ ref('silver_bookings') }}
